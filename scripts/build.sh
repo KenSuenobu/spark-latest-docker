3ea8501397362e72c37e6b4dfea505239b3a911a
ksuenobu <ksuenobu@fastmail.com>
2018-06-05 22:14:14 -0500

    Removed save of artifact for Circle - not necessary.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,3 @@
 # Script to build Docker Container for spark-latest
 
 docker build -t ksuenobu/spark-latest .
-
-docker save ksuenobu/spark-latest > spark-latest.tar
-gzip spark-latest.tar
-mv spark-latest.tar.gz /tmp
